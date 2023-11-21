ce2e19ad91c41fc69e08e73866bf5e9ed8fc7743
omnit3a <fostyr@debian>
2023-11-21 11:14:18 -0500

    .

--- emacs/presley-mode.el
+++ emacs/presley-mode.el
@@ -1,8 +1,8 @@
 
 (define-generic-mode presley-mode
   nil
-  (list "def-func" "let" "lambda", "if",
-	"cond", "else", "case")
+  (list "def-func" "let" "lambda" "if"
+	"cond" "else" "case")
   '(("true" . 'font-lock-constant-face)
     ("false" . 'font-lock-constant-face))
   (list "\\.pre$")
